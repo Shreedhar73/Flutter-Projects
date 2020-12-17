92235c553e4fe67c42eb7ace51f3cae0b16f67ab
Shreedhar73 <shreedharpandey000@gmail.com>
2020-12-17 15:36:34 +0545

    Stateless/stateful widget, hot restart hot reload

--- lib/main.dart
+++ lib/main.dart
@@ -2,21 +2,42 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: Home(),
+
+  ));
+}
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text('Fallen700'),
+        title: Text(
+          "Fallen700",
+          style: TextStyle(
+            color: Colors.cyanAccent,
+            fontSize: 30.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            fontFamily: 'IndieFlower',
+          ),
+        ),
         centerTitle: true,
+        backgroundColor: Colors.red[600],
       ),
 
       body: Center(
         child: Text('Welcome to the world'),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Text('Click'),
+        onPressed: () {},
+        child: Text('Click '),
+        backgroundColor: Colors.red[600],
       ),
-
-    ),
-
-  ));
+    );
+  }
 }
+
+
+
 
